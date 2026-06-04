8b637eefc99c8f15987c5e9dc6c4a3d761ee9e61
Stimpy <95110502+StimpyDev@users.noreply.github.com>
2026-06-04 23:30:19 +0200

    Update MultiPlayerGameModeMixin.java

--- src/client/java/me/incend1um/noinputlagtickrate/mixin/client/MultiPlayerGameModeMixin.java
+++ src/client/java/me/incend1um/noinputlagtickrate/mixin/client/MultiPlayerGameModeMixin.java
@@ -11,31 +11,35 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MultiPlayerGameMode.class)
 public class MultiPlayerGameModeMixin {
-	@Shadow @Final private Minecraft minecraft;
-	@Shadow private float destroyTicks;
-	@Shadow private int destroyDelay;
+    @Shadow @Final private Minecraft minecraft;
+    @Shadow private float destroyTicks;
+    @Shadow private int destroyDelay;
 
-	@Unique
-	long lastTick = -1;
+    @Unique
+    private long lastTick = -1;
 
-	@Redirect(method = "continueDestroyBlock", at = @At(value = "FIELD", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;destroyDelay:I", opcode = Opcodes.PUTFIELD, ordinal = 0))
-	void syncDestroyingDelayWithActualTickRate(MultiPlayerGameMode instance, int value) {
-		if (((MinecraftAccess) this.minecraft).getClientTickCount() != lastTick) {
-			this.destroyDelay--;
-		}
-	}
+    @Redirect(method = "continueDestroyBlock", at = @At(value = "FIELD", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;destroyDelay:I", opcode = Opcodes.PUTFIELD, ordinal = 0))
+    void syncDestroyingDelayWithActualTickRate(MultiPlayerGameMode instance, int value) {
+        long currentTick = ((MinecraftAccess) this.minecraft).getClientTickCount();
+        if (currentTick != lastTick) {
+            this.destroyDelay = value;
+        }
+    }
 
-	@ModifyExpressionValue(method = "continueDestroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getDestroyProgress(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)F"))
-	float syncDestroyingWithActualTickRate(float original) {
-		return original * (50.0f / this.minecraft.level.tickRateManager().millisecondsPerTick());
-	}
+    @ModifyExpressionValue(method = "continueDestroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getDestroyProgress(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)F"))
+    float syncDestroyingWithActualTickRate(float original) {
+        if (this.minecraft.level != null) {
+            return original * (50.0f / this.minecraft.level.tickRateManager().millisecondsPerTick());
+        }
+        return original;
+    }
 
-	@Redirect(method = "continueDestroyBlock", at = @At(value = "FIELD", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;destroyTicks:F", opcode = Opcodes.PUTFIELD, ordinal = 0))
-	void syncSoundWithActualTickRate(MultiPlayerGameMode instance, float value) {
-		long currentTick;
-		if ((currentTick = ((MinecraftAccess) this.minecraft).getClientTickCount()) != lastTick) {
-			lastTick = currentTick;
-			this.destroyTicks++;
-		}
-	}
+    @Redirect(method = "continueDestroyBlock", at = @At(value = "FIELD", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;destroyTicks:F", opcode = Opcodes.PUTFIELD, ordinal = 0))
+    void syncSoundWithActualTickRate(MultiPlayerGameMode instance, float value) {
+        long currentTick = ((MinecraftAccess) this.minecraft).getClientTickCount();
+        if (currentTick != lastTick) {
+            this.lastTick = currentTick;
+            this.destroyTicks = value;
+        }
+    }
 }
